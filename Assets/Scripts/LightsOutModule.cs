3a923d625ca896a78af5767bea293ae72eed1ca3
Brendan King <brendan@brendanking.com>
2024-01-26 21:24:00 -0500

    Reuploaded module

---
+++ Assets/Scripts/LightsOutModule.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class LightsOutModule : MonoBehaviour
+{
+    public KMSelectable Selectable;
+    public KMNeedyModule NeedyModule;
+
+    private LightsOutButton[] ChildButtons
+    {
+        get
+        {
+            return GetComponentsInChildren<LightsOutButton>();
+        }
+    }
+
+    private bool AllLightsOff
+    {
+        get
+        {
+            return ChildButtons.All((x) => !x.LitState);
+        }
+    }
+
+    private void Start()
+    {
+        NeedyModule.OnNeedyActivation += OnActivate;
+        NeedyModule.OnNeedyDeactivation += OnDeactivate;
+        NeedyModule.OnTimerExpired += OnTimerExpired;
+
+        foreach (KMSelectable selectable in Selectable.Children)
+        {
+            selectable.OnInteract += OnButtonInteract;
+        }
+	}
+
+    private void OnDestroy()
+    {
+        NeedyModule.OnNeedyActivation -= OnActivate;
+        NeedyModule.OnNeedyDeactivation -= OnDeactivate;
+        NeedyModule.OnTimerExpired -= OnTimerExpired;
+
+        foreach (KMSelectable selectable in Selectable.Children)
+        {
+            selectable.OnInteract -= OnButtonInteract;
+        }
+    }
+
+    private void OnActivate()
+    {
+        while (AllLightsOff)
+        {
+            RandomizeLights();
+        }
+
+        EnableLights(true);
+    }
+
+    private void OnDeactivate()
+    {
+        ResetLights();
+        EnableLights(false);
+    }
+
+    private void OnTimerExpired()
+    {
+        if (!AllLightsOff)
+        {
+            NeedyModule.HandleStrike();
+        }
+
+        ResetLights();
+        EnableLights(false);
+    }
+
+    private bool OnButtonInteract()
+    {
+        if (AllLightsOff)
+        {
+            NeedyModule.HandlePass();
+            EnableLights(false);
+        }
+
+        return false;
+    }
+
+    private void ResetLights()
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.SetState(false);
+        }
+    }
+
+    private void RandomizeLights()
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.SetState(UnityEngine.Random.Range(0, 2) == 1);
+        }
+    }
+
+    private void EnableLights(bool enable)
+    {
+        foreach (LightsOutButton button in ChildButtons)
+        {
+            button.CanInteract = enable;
+        }
+    }
+
+    private KMSelectable[] ProcessTwitchCommand(string command)
+    {
+        if (!command.StartsWith("press ", StringComparison.InvariantCultureIgnoreCase))
+        {
+            return null;
+        }
+
+        command = command.Substring(5);
+
+        string[] sequence = command.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<KMSelectable> selectables = new List<KMSelectable>();
+        foreach(string buttonString in sequence)
+        {
+            int buttonIndex = -1;
+            if (int.TryParse(buttonString, out buttonIndex) && buttonIndex >= 1 && buttonIndex <= 9)
+            {
+                selectables.Add(Selectable.Children[buttonIndex - 1]);
+            }
+        }
+
+        return selectables.ToArray();
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Module by Bashly<br/>
+Maintained by eXish<br/>
+<br/>
+==Steam Workshop Link==<br/>
+https://steamcommunity.com/sharedfiles/filedetails/?id=3147989432<br/>
+<br/>
+==Manual Links==<br/>
+HTML: https://ktane.timwi.de/HTML/Lights%20Out.html<br/>
+PDF: https://ktane.timwi.de/PDF/Lights%20Out.pdf<br/>
